6c63f42a0c36887fd8a12a59fc3182c106ebb953
Robert Jernberg <robert.jernberg@gmail.com>
2017-12-12 21:13:54 +0100

    Finaly got the chart to refresh.

--- TestCoreWebApp/ClientApp/app/components/loans/loancharts/loancharts.component.ts
+++ TestCoreWebApp/ClientApp/app/components/loans/loancharts/loancharts.component.ts
@@ -1,5 +1,4 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { DataSource } from '@angular/cdk/collections';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import { LoanService, LoanPayment } from '../services/loan.service';
@@ -15,8 +14,6 @@ import { HostListener } from '@angular/core';
 export class LoanChartsComponent implements OnInit {
     public loanpayments: LoanPayment[];
     
-    dataSource = new ExampleDataSource(this.loanpayments);
-
     constructor(private loanService: LoanService) {
         this.loanpayments = new Array();
     }
@@ -31,17 +28,7 @@ export class LoanChartsComponent implements OnInit {
         dataTable: [
             ['Date', 'Principal', 'Interest'],
             ['2017-10-31', 79.59, 8.33],
-            //['2017-11-30', 80.25, 7.67],
-            //['2017-12-31', 80.92, 7.00],
-            //['2018-01-31', 81.59, 6.33],
-            //['2018-02-28', 82.27, 5.65],
-            //['2018-03-31', 82.96, 4.96],
-            //['2018-04-30', 83.65, 4.27],
-            //['2018-05-31', 84.35, 3.57],
-            //['2018-06-30', 85.05, 2.87],
-            //['2018-07-31', 85.76, 2.16],
-            //['2018-08-31', 86.47, 1.45],
-            //['2018-09-30', 87.14, 0.73],
+            ['2017-11-30', 80.25, 7.67],
         ],
         options: {
             'title': 'Tasks',
@@ -52,75 +39,34 @@ export class LoanChartsComponent implements OnInit {
     private processYourData() {
         var data = [
             ['Date', 'Principal', 'Interest'],
-            ['2017-10-31', 179.59, 8.33],
-            ['2017-11-30', 80.25, 7.67],
-            ['2017-12-31', 80.92, 7.00],
-            ['2018-01-31', 81.59, 6.33],
-            ['2018-02-28', 82.27, 5.65],
-            ['2018-03-31', 82.96, 4.96],
-            ['2018-04-30', 83.65, 4.27],
-            ['2018-05-31', 84.35, 3.57],
-            ['2018-06-30', 85.05, 2.87],
-            ['2018-07-31', 85.76, 2.16],
-            ['2018-08-31', 86.47, 1.45],
-            ['2018-09-30', 87.14, 0.73],
+            ['2017-10-31', 179.59, 8.33], // Bull shit row to get right date type.
         ];
-        data.length = 0;
-        data.push(['Date', 'Principal', 'Interest']);
-        data.push(['2017-10-31', 179.59, 8.33]);
-        data.push(['2017-10-31', 99.59, 28.33]);
-        console.log(this.loanpayments);
+        data.length = 1
         for (let entry of this.loanpayments) {
             data.push([entry.periodFormatted, entry.principal, entry.interest]);
         }
-        //for (let entry of data) {
-        //    console.log(typeof entry[0])
-        //    console.log(typeof entry[1])
-        //    console.log(typeof entry[2])
-        //}
-        var data2 = [
-            ['Date', 'Principal', 'Interest'],
-            ['2017-10-31', 179.59, 8.33],
-            ['2017-11-30', 80.25, 7.67],
-            ['2017-12-31', 80.92, 7.00],
-            ['2018-01-31', 81.59, 6.33],
-            ['2018-02-28', 82.27, 5.65],
-            ['2018-03-31', 82.96, 4.96],
-            ['2018-04-30', 83.65, 4.27],
-            ['2018-05-31', 84.35, 3.57],
-            ['2018-06-30', 85.05, 2.87],
-            ['2018-07-31', 85.76, 2.16],
-            ['2018-08-31', 86.47, 1.45],
-            ['2018-09-30', 87.14, 0.73],
-        ];
-        console.log(data);
-        console.log(data2);
-        console.log(typeof data);
-        console.log(typeof data2);
-        this.chartData.dataTable = data;
+
+
+        if (this.loanpayments.length > 1) {
+            this.showSpinner = false;
+            this.chartData = {
+                chartType: 'AreaChart',
+                dataTable: data,
+                options: {
+                    'title': 'Tasks',
+                    'isStacked': 'true',
+                },
+            };
+        }
+        
     }
 
     ngOnInit() {
         this.showSpinner = true;
         this.loanService.currentLoanPayments.subscribe(lp => {
-            this.showSpinner = false;
             this.loanpayments = lp;
-            this.dataSource = new ExampleDataSource(lp)
             this.processYourData();
         });
     }
 
-}
-
-class ExampleDataSource extends DataSource<any> {
-    /** Connect function called by the table to retrieve one stream containing the data to render. */
-    constructor(private loanpayments: LoanPayment[]) {
-        super();
-    }
-
-    connect(): Observable<LoanPayment[]> {
-        return Observable.of(this.loanpayments);
-    }
-
-    disconnect() { }
 }
